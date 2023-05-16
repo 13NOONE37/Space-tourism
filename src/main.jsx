3b22a303e629319493fca43c37289bf7a0242413
13NOONE37 <oliwerklauze22@gmail.com>
2023-05-16 22:20:43 +0200

    build fixed

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom/client';
 
 import './config.css';
 import App from './components/app/App';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
+      {/* <BrowserRouter> */}
       <App />
-    </BrowserRouter>
+      {/* </BrowserRouter> */}
+    </HashRouter>
   </React.StrictMode>,
 );
